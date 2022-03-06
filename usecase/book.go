0a874d2253294880aad8f09723505d71594c0255
Asef264 <xhmoazedi@gmail.com>
2022-03-06 13:58:58 +0330

    using clean architecture , adding DIPs and add new book method

---
+++ usecase/book.go
@@ -0,0 +1,46 @@
+package usecase
+
+import (
+	"context"
+	"log"
+	"my_lib/errorhandler"
+	"my_lib/models"
+	"my_lib/repository"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UsecaseInterface interface {
+	AddBook(ctx context.Context, bookDate models.Book) (newbook *models.Book, err error)
+}
+
+type book struct {
+	repo repository.RepoInterface
+}
+
+func NewUsecaseInterface(repo repository.RepoInterface) UsecaseInterface {
+	return &book{
+		repo: repo,
+	}
+}
+
+func (b *book) AddBook(ctx context.Context, bookData models.Book) (newbook *models.Book, err error) {
+	bookData.AddTime = time.Now()
+	bookData.Id = uuid.New().String()
+	err = b.repo.AddBook(ctx, models.Book{
+		Name:       bookData.Name,
+		Id:         bookData.Id,
+		Category:   bookData.Category,
+		Author:     bookData.Author,
+		Translator: bookData.Translator,
+		Owner:      bookData.Owner,
+		AddTime:    bookData.AddTime,
+	})
+	if err != nil {
+		log.Println(errorhandler.LayerConnectionError + "usecase")
+		log.Println("error on setting data in object in usecase")
+		return nil, err
+	}
+	return &bookData, nil
+}
